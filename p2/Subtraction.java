fd4721e6749223cce3181b9dadb0a35ab5f98592
Shivam Gravity <shivamgravity@gmail.com>
2025-08-31 00:56:33 +0530

    added new files

---
+++ p2/Subtraction.java
@@ -0,0 +1,10 @@
+package p2;
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int diff;
+        diff = a - b;
+        System.out.println(a + " - " + b + " = " + diff);
+    }
+}
