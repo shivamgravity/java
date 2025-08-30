9337874f67ed51e943e9ae181537b5414795d4cf
Shivam Gravity <shivamgravity@gmail.com>
2025-08-31 01:20:02 +0530

    added new files

---
+++ p2/Multiplication.java
@@ -0,0 +1,10 @@
+package p2;
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int product;
+        product = a * b;
+        System.out.println(a + " x " + b + " = " + product);
+    }
+}
