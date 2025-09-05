69977f715f52d7f8b9324e1ed66c4c15aa2c6b8d
Shivam Gravity <shivamgravity@gmail.com>
2025-09-05 08:50:47 +0530

    added Double.java in p3

---
+++ p3/Double.java
@@ -0,0 +1,11 @@
+package p3;
+
+public class Double {
+    public static void main(String[] args) {
+        double a, b, c;
+        a = 400000;
+        b = 100000;
+        c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
