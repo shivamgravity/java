2784dbf7a40404dd0d3752c051eee0ad2fe136a5
Shivam Gravity <shivamgravity@gmail.com>
2025-09-05 08:42:13 +0530

    added Float.java in p3

---
+++ p3/Float.java
@@ -0,0 +1,11 @@
+package p3;
+
+public class Float {
+    public static void main(String[] args) {
+        float a, b, c;
+        a = 12.5f;
+        b = 12.5f;
+        c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
