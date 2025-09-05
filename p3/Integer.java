14a6b5d677831576194c63b8ec27f7fe42f7c5c0
Shivam Gravity <shivamgravity@gmail.com>
2025-09-05 08:35:03 +0530

    added Integer.java in p3

---
+++ p3/Integer.java
@@ -0,0 +1,11 @@
+package p3;
+
+public class Integer {
+    public static void main(String[] args) {
+        int a, b;
+        a = 1000;
+        b = 3000;
+        int c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
