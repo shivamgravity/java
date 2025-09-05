c2d298606deca138f093cf15c343bf8a995d4cc3
Shivam Gravity <shivamgravity@gmail.com>
2025-09-05 20:22:10 +0530

    updated temp.java

--- __temp/temporary.java
+++
@@ -1,7 +0,0 @@
-package __temp;
-
-public class temporary {
-    public static void main(String[] args) {
-        System.out.println("Just the temprary file to run random commands...");
-    }
-}
---
+++ p0/temp.java
@@ -0,0 +1,17 @@
+package p0;
+
+public class temp {
+    public static void main(String[] args) {
+        System.out.println("Just the temprary file to run random commands...");
+
+        byte b;
+        int a, c;
+        a = 250;
+        b = (byte) a;
+        System.out.println(b);
+
+        c = 257;
+        b = (byte) c;
+        System.out.println(b);
+    }
+}
