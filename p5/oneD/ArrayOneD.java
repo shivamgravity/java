d8a16249a12cbe19b7c11089ea08f710e1e3f992
Shivam Gravity <shivamgravity@gmail.com>
2025-09-06 09:14:48 +0530

    added ArrayOneD.java in p5.oneD

---
+++ p5/oneD/ArrayOneD.java
@@ -0,0 +1,18 @@
+package p5.oneD;
+
+public class ArrayOneD {
+    public static void main(String[] args) {
+        int arr[];
+        arr = new int[5];
+        
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+
+        for(int i=0; i<5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
