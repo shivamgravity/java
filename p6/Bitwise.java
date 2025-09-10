211468b31dd9e0279e095f8fae7eb4f4a8ee9703
Shivam Gravity <shivamgravity@gmail.com>
2025-09-10 23:44:36 +0530

    added Bitwise.java file

---
+++ p6/Bitwise.java
@@ -0,0 +1,10 @@
+package p6;
+
+public class Bitwise {
+    public static void main(String[] args) {
+        // bitwise unary NOT
+        int a = 25;
+        int aa = ~a;
+        System.out.println(aa);
+    }
+}
