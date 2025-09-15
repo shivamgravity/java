9ec5217f47fddc270f95742e62667a43fdd34aab
Shivam Gravity <shivamgravity@gmail.com>
2025-09-15 11:25:00 +0530

    added bitwise logical operators

--- p6/Bitwise.java
+++ p6/Bitwise.java
@@ -2,9 +2,30 @@ package p6;
 
 public class Bitwise {
     public static void main(String[] args) {
-        // bitwise unary NOT
+        // common variables
         int a = 25;
-        int aa = ~a;
-        System.out.println(aa);
+        int b = 30;
+
+        // Logical Operators: & | ^ ~
+
+        // ~ bitwise unary NOT
+        // reverses all the bits
+        int na = ~a;
+        System.out.println("~" + a + " = " + na);
+
+        // & bitwise AND
+        // gives 1 only if both bits are 1
+        int aANDb = a & b;
+        System.out.println(a + " & " + b + " = " + aANDb);
+
+        // | bitwise OR
+        // gives 1 if at least 1 is present
+        int aORb = a | b;
+        System.out.println(a + " | " + b + " = " + aORb);
+
+        // ^ exclusive OR
+        // gives 1 if one is 0 and other is 1 else 0
+        int aXORb = a ^ b;
+        System.out.println(a + " ^ " + b + " = " + aXORb);
     }
 }
