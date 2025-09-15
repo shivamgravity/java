b8d4bdb554c7564419e260b40a0b01bde670f04b
Shivam Gravity <shivamgravity@gmail.com>
2025-09-15 13:52:33 +0530

    added left shift operator

--- p6/Bitwise.java
+++ p6/Bitwise.java
@@ -5,6 +5,7 @@ public class Bitwise {
         // common variables
         int a = 25;
         int b = 30;
+        byte c = 10;
 
         // Logical Operators: & | ^ ~
 
@@ -27,5 +28,15 @@ public class Bitwise {
         // gives 1 if one is 0 and other is 1 else 0
         int aXORb = a ^ b;
         System.out.println(a + " ^ " + b + " = " + aXORb);
+
+        // Logical Operators ENDS ------
+
+        // << left shift (value << num)
+        // shifts the bits, num times to left side
+        // be careful about how many bits each data type have
+        int shift1a = c << 2;
+        byte shift2a = (byte) (c << 2);
+        System.out.println(c + " << 2 (converted to int) = " + shift1a);
+        System.out.println(c + " << 2 (byte as original) = " + shift2a);
     }
 }
